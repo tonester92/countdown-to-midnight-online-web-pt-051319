4b64d831179d58a7cc72aa54abeaf64e16652288
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-18 23:18:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
-
-def countdown
+def countdown(number)
+  while number < 10000000
+  number =- 1 
+  puts "#{number} SECOND(S)!"
+  if number == 0
+  puts "HAPPY NEW YEAR!"
 end
+
+countdown_with_sleep
+  
+
+
+countdown(49)
